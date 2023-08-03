f30989d88d96d0527c908a0a3fc2d0d509be328c
valeriecalle <valerie.calle@icloud.com>
2023-08-02 21:54:11 -0500

    fifthcode

--- src/components/About/aboutme.js
+++ src/components/About/aboutme.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Card, CardContent, CardMedia, Typography, makeStyles } from "@material-ui/core";
+import { Card, CardActions, CardContent, CardMedia, Typography, makeStyles } from "@material-ui/core";
 import vcalle from "../../images/vcalle.jpg";
-// import TypeWriter from "react-typewriter";
+// import { TypeWriterEffect } from "react-typewriter-effect";
+import TypeWriterEffect from "react-typewriter-effect";
+
+
 
 
 const Aboutme = ({id, title, dark }) => {
@@ -14,18 +17,29 @@ const Aboutme = ({id, title, dark }) => {
           <Card className={classes.card}>
             <CardMedia image={vcalle} className={classes.cmedia} title="picture" />
 
-              <CardContent className={classes.cardcontent}>
-                Contenido referente a la profesión de Valerie. y su proyectos                {/* <TypeWriter
-                
-                  text="Valerie Calle"
-                  textStyle={{ fontSize: "2rem", fontWeight: "700px", color: "#026FEB" }}
-                />
-                {/* <TypeWriter
-                  text="Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas"
-                  textStyle={{ fontSize: "1rem", fontWeight: "10px", color: "#D41B06" }}
-                />  */} 
+            <CardContent className={classes.cardcontent}>
+              <TypeWriterEffect
+                text="Valerie Calle"
+                textStyle={{ fontSize: "3rem", fontWeight: "600px", color: "#A78295" }}
 
-              </CardContent>
+              />  
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus. Accumsan in nisl nisi scelerisque eu. Maecenas accumsan lacus vel facilisis. Malesuada bibendum arcu vitae elementum curabitur vitae. Purus non enim praesent elementum facilisis leo. Sit amet consectetur adipiscing elit duis. Turpis egestas integer eget aliquet. Ridiculus mus mauris vitae ultricies leo. Vel turpis nunc eget lorem dolor sed viverra. Facilisis mauris sit amet massa vitae. Vitae congue eu consequat ac felis donec et odio pellentesque. At varius vel pharetra vel turpis nunc eget.
+
+                Aliquet nibh praesent tristique magna sit. Adipiscing enim eu turpis egestas pretium aenean pharetra.
+                Consectetur purus ut faucibus pulvinar elementum integer enim. Dictum sit amet justo donec enim. Morbi quis commodo odio aenean sed. </p>
+
+            </CardContent>
+            <CardActions>
+
+              {/* <Button variant = "contained" className={classes.pdfbutton}>
+                <a href = { } download>
+                  Download CV
+                </a>
+              </Button> */}
+
+
+            </CardActions>
 
 
           </Card>
@@ -52,7 +66,7 @@ const useStyles = makeStyles ((theme)=>({
       
     },
     card: {
-      width: "650px",
+      width: "80%",
       height:"450px",
       display: "flex",
       marginTop: theme.spacing(5),
@@ -62,7 +76,7 @@ const useStyles = makeStyles ((theme)=>({
     },
 
     cmedia:{
-      width: "250px",
+      width: "100vw",
       height: "auto",
       ObjectFit: "cover",
       margin: theme.spacing(3),
